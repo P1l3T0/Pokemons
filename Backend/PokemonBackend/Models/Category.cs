2ba478e37229ce6276daab42316e9ec10d6c807a
P1l3T0 <vgan3v@gmail.com>
2023-11-26 20:12:32 +0200

    add many-to-many relationships

--- Backend/PokemonBackend/Models/Category.cs
+++ Backend/PokemonBackend/Models/Category.cs
@@ -1,8 +1,9 @@
-﻿namespace PokemonWebAPI.Models
+﻿namespace PokemonBackend.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public ICollection<PokemonCategory>? PokemonCategories { get; set; }
     }
 }
---
+++ Backend/PokemonBackend/Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonBackend.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int OwnerId{ get; set; }
+        public Pokemon? Category { get; set; }
+        public Owner? Owner { get; set; }
+    }
+}
