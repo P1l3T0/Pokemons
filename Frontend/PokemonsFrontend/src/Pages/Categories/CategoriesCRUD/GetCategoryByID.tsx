648cbb89096accf9bf8b42e72ff0f3a73042d627
P1l3T0 <vgan3v@gmail.com>
2023-12-28 15:54:36 +0200

    feat(GetCategoryByID): add new component

---
+++ Frontend/PokemonsFrontend/src/Pages/Categories/CategoriesCRUD/GetCategoryByID.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import { categoriesEndPoint } from "../../../endpoints";
+import GetDataByID from "../../Helpers/GetDataByID";
+import ResponseMessages from "../../Helpers/ResponseMessages";
+
+const GetCategoryById = () => {
+  const [id, setId] = useState(0);
+  const [error, setError] = useState(false);
+  const [initiallyClicked, setInitiallyClicked] = useState(false);
+  const [category, setCategory] = useState<CategoryObject>();
+
+  const getCountryByIdAsync = async () => {
+    await axios
+      .get(`${categoriesEndPoint}/${id}`)
+      .then((res: AxiosResponse<CategoryObject>) => {
+        setCategory(res.data);
+        setError(false);
+        !initiallyClicked ? setInitiallyClicked(true) : "";
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
+  };
+
+  return (
+    <>
+      <div className="container">
+        <GetDataByID
+          setId={setId}
+          httpMethod={"get"}
+          buttonText={"Get category by ID"}
+          onClick={getCountryByIdAsync}
+        />
+
+        <ResponseMessages
+          error={error}
+          data={category}
+          initiallyClicked={initiallyClicked}
+          errorMessage="Enter a valid ID!"
+        />
+      </div>
+    </>
+  );
+};
+
+export default GetCategoryById;
